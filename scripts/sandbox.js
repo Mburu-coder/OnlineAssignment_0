48caf590e062768c19980c2114f06ffa2fdbb082
Mburu-coder <60397100+Mburu-coder@users.noreply.github.com>
2020-09-09 14:09:17 +0300

    Uploading Files

---
+++ scripts/sandbox.js
@@ -0,0 +1,68 @@
+"use strict";
+
+var restaurants = L.layerGroup();
+
+var mapboxAttribution = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> Contributors: ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Mburu Eileen ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>' ,
+		mapboxURL = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWJ1cnVtYnVydSIsImEiOiJja2RlMmRxa2EwcHlvMnRxcXJvcm13dWxqIn0.LVTLGjL-WDQh9LXOvcan7A';
+
+//Creating  base layers and adding the default ones to the map
+var grayscale = L.tileLayer(mapboxURL, {id: 'mapbox/light-v9', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution}),
+    streets = L.tileLayer(mapboxURL, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution}),
+    OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {maxZoom: 10,});
+
+//Map Initialisation
+var map = L.map('map', {
+    center: [0.167, 37.782],
+    zoom: 10,
+    layers: [grayscale, restaurants]
+    });
+
+//Creating 2 objects: Our Baselayers and Overlays
+var baseLayers = {
+    "GrayScale": grayscale,
+    "Streets": streets,
+    "OSM B&W": OpenStreetMap_BlackAndWhite
+};
+
+var overlays = {
+    "Restaurants": restaurants
+};
+
+//Layer Control
+L.control.layers(baseLayers, overlays).addTo(map);
+
+//Style the Keys
+var baseMaps = {
+    "<span style='color: gray'>Grayscale</span>": grayscale,
+    "Streets": streets
+};
+
+//Loading JSON from and external file
+var promise = $.getJSON("JSON/restaurants.geojson");
+promise.then(function(data) {
+    var allrestaurants = L.geoJson(data);
+
+    var restaurants = L.geoJson(data, {
+        pointToLayer: function(feature, latlng) {
+            return L.marker(latlng, {
+                icon: restaurantIcon
+            }).on('mouseover', function(){
+                this.bindPopup('Restaurant Name: ' + feature.properties.name + '<br>' + 'Cuisine: ' + feature.properties.cuisine).openPopup();
+            });
+        }
+    }).addTo(map);
+
+    map.fitBounds(allrestaurants.getBounds(), {
+        padding: [50, 50]
+    })
+    console.log(data) // take a look at the data in the console
+});
+
+//Creating Restaurant Icon
+var restaurantIcon = L.AwesomeMarkers.icon({
+    prefix: 'fa', //font awesome rather than bootstrap
+    markerColor: 'blue', //Possible: red, darkred, orange, green, darkgreen, blue, purple, dark purple
+    icon: 'coffee' //https://fontawesome.com/icons?from=io
+});
